3c040b6492fcabd74d1f66ce828f5584152b890b
= <=>
2025-10-08 09:11:44 -0500

    animales

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def base():
-    return render_template("base.html")
+def inicio():
+    return render_template("inicio.html")
 
 @app.route('/animalesexoticos')
 def animales():
